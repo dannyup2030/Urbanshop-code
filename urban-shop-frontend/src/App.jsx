258331866944ddfb74f29e02c3809507486a63ec
danny up <dannyup8@hotmail.com>
2026-02-27 10:38:28 -0500

    actualizacion base de datos

--- urban-shop-frontend/src/App.jsx
+++ urban-shop-frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import Inicio from "./pages/Inicio.jsx";
 import Registro from "./pages/Registro.jsx";
 import Sesion from "./pages/Sesion.jsx";
@@ -12,7 +11,6 @@ import Sistema from "./pages/Sistema.jsx";
 function App() {
   return (
     <HashRouter>
-    <HashRouter>
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/registro" element={<Registro />} />
@@ -22,9 +20,8 @@ function App() {
         <Route path="/pedidos" element={<Pedidos />} />
         <Route path="/sistema" element={<Sistema />} />
         <Route path="*" element={<Navigate to="/" replace />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
-      </HashRouter>
+    </HashRouter>
   );
 }
 
